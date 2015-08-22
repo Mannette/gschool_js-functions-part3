9446c97e02ef29c0eeaa51e5c1bba733318470b2
ethan <ethanmannette@gmail.com>
2015-08-21 22:51:16 -0600

    Finished exercises up to getting an array at a negative index. Added jasmine testing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
 // add scripts
 
 console.log("sanity check!");
+
+var student = {name: 'Michael', age: 27};
+function getStudentName (student) {
+  return student.name;
+}
+console.log(getStudentName(student));
+
+var stringArray = ['This', 'is', 'a', 'sentence'];
+function getTotalLetters (arr) {
+  var joined = arr.join('');
+  var split = joined.split('');
+  return split.length;
+}
+console.log(getTotalLetters(stringArray));
+
+var newObject = {};
+function createObject (objKey, objValue) {
+  newObject[objKey] = objValue;
+  return newObject;
+}
+createObject ('city', 'Denver');
+console.log(newObject);
+
+var letterArray = ['a', 'b', 'c', 'd', 'e'];
+function getNegativeIndex (arr, num) {
+  return arr[arr.length + num];
+}
+console.log(getNegativeIndex(letterArray, -4));
+
+module.exports = {
+  'getStudentName': getStudentName,
+  'getTotalLetters': getTotalLetters,
+  'createObject': createObject,
+  'getNegativeIndex': getNegativeIndex
+};
--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,38 @@
 var code = require('../js/main.js');
 
+var student1 = {name: 'Ethan', age: 23}
 
-// describe('', function() {
-//   it('', function() {
+describe('Get Name', function() {
+  it('should return the value of the name property', function() {
+    expect(code.getStudentName(student1)).toEqual('Ethan');
+  });
+});
 
-//   });
-// });
+
+var stringArr = ['This', 'is', 'to', 'test'];
+
+describe('Get Letters', function() {
+  it('should return total number of letters', function() {
+    expect(code.getTotalLetters(stringArr)).toEqual(12);
+  });
+});
+
+describe('Create Object', function() {
+  it('should return a new object with a key of the first argument and the value of the second argument', function() {
+    // code.createObject('city', 'Denver');
+    expect(code.createObject('city', 'Denver')).toEqual({city: 'Denver'});
+  });
+});
+
+var letterArray = ['a', 'b', 'c', 'd', 'e'];
+describe('Negative Index', function() {
+  it('should return the value from the array at the given negative index', function() {
+    expect(code.getNegativeIndex(letterArray, -2)).toEqual('d');
+  });
+});
+
+describe('Remove Character', function() {
+  it('should remove characters passed as the second argument', function() {
+    expect(code.removeCharacter()).toEqual();
+  });
+});
