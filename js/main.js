2938df4ca1c966ef375f8218dd4ff6a791d0b547
ethan <ethanmannette@gmail.com>
2015-08-24 11:47:41 -0600

    Finished seventh function with jasmine tests.

--- js/main.js
+++ js/main.js
@@ -2,42 +2,153 @@
 
 console.log("sanity check!");
 
+// -------------- //
+// FIRST FUNCTION //
+// -------------- //
+// create student
 var student = {name: 'Michael', age: 27};
 function getStudentName (student) {
+  // return name value
   return student.name;
 }
 console.log(getStudentName(student));
 
+// --------------- //
+// SECOND FUNCTION //
+// --------------- //
+// create array of strings
 var stringArray = ['This', 'is', 'a', 'sentence'];
 function getTotalLetters (arr) {
+  // join strings to remove white space
   var joined = arr.join('');
+  // split joined string
   var split = joined.split('');
+  // return length
   return split.length;
 }
 console.log(getTotalLetters(stringArray));
 
+// -------------- //
+// THIRD FUNCTION //
+// -------------- //
+// create object
 var newObject = {};
 function createObject (objKey, objValue) {
+  // make params as key and value
   newObject[objKey] = objValue;
+  // return object
   return newObject;
 }
 createObject ('city', 'Denver');
 console.log(newObject);
 
+// --------------- //
+// FOURTH FUNCTION //
+// --------------- //
+// create array of letters
 var letterArray = ['a', 'b', 'c', 'd', 'e'];
 function getNegativeIndex (arr, num) {
+  // return index of length + negNum
   return arr[arr.length + num];
 }
-console.log(getNegativeIndex(letterArray, -4));
+getNegativeIndex(letterArray, -4);
 
+// -------------- //
+// FIFTH FUNCTION //
+// -------------- //
+// create string
 var toEdit = 'Hello there';
-function editString (str, char) {
-  
+function removeCharacter (str, char) {
+  // cache split string with removed character
+  var cached = str.split(char).join('');
+  // return cached string
+  return cached;
+}
+removeCharacter(toEdit, 'e');
+
+// -------------- //
+// SIXTH FUNCTION //
+// -------------- //
+// create object
+var nameObj = {jimmy: 15, greg: 20, tim: 14, cindy: 8};
+function outputObject (obj) {
+  // create array to cache each key and value
+  var result = [];
+  // for-in to loop through entire object
+  for (i in obj) {
+    // push key and value to array
+    result.push(i + ' is ' + obj[i]);
+    // join array with comma to make single string
+    var final = result.join(', ');
+  }
+  return final;
+}
+console.log(outputObject(nameObj));
+
+// ---------------- //
+// SEVENTH FUNCTION //
+// ---------------- //
+// create a string
+var daString = 'A string with some vowels';
+function getVowels (str) {
+  // create array with vowels
+  var vowelArr = ['a', 'e', 'i', 'o', 'u'];
+  // create array to hold present vowels
+  var stringVowels = [];
+  // convert string to lowercase
+  var lowerStr = str.toLowerCase();
+  // split the string at each character
+  var splitStr = lowerStr.split('');
+  // for-in to loop through entire array
+  for (i in splitStr) {
+    // check index of each vowel within string passed to function
+    if (splitStr.indexOf(vowelArr[i]) !== -1) {
+      // push vowels present in string to new array
+      stringVowels.push(vowelArr[i]);
+    }
+  }
+  // return new array
+  return stringVowels;
+}
+console.log(getVowels(daString));
+
+// --------------- //
+// EIGHTH FUNCTION //
+// --------------- //
+// create array that will pass
+var passArr = [];
+// create array that will fail
+var failArr = [];
+function captureTwins (arr) {
+
 }
 
+// -------------- //
+// NINTH FUNCTION //
+// -------------- //
+function testBooleanLogic (arr) {
+
+}
+
+// -------------- //
+// TENTH FUNCTION //
+// -------------- //
+function getUniqueValues (arr) {
+
+}
+
+// -------------- //
+// MODULE EXPORTS //
+// -------------- //
 module.exports = {
   'getStudentName': getStudentName,
   'getTotalLetters': getTotalLetters,
   'createObject': createObject,
-  'getNegativeIndex': getNegativeIndex
+  'getNegativeIndex': getNegativeIndex,
+  'removeCharacter': removeCharacter,
+  'outputObject': outputObject,
+  'getVowels': getVowels,
+  'captureTwins': captureTwins,
+  'testBooleanLogic': testBooleanLogic,
+  'getUniqueValues': getUniqueValues
 };
--- spec/spec.js
+++ spec/spec.js
@@ -1,9 +1,9 @@
 var code = require('../js/main.js');
 
-var student1 = {name: 'Ethan', age: 23}
+var student1 = {name: 'Ethan', age: 23};
 
-describe('Get Name', function() {
-  it('should return the value of the name property', function() {
+describe('Get Name', function () {
+  it('should return the value of the name property', function () {
     expect(code.getStudentName(student1)).toEqual('Ethan');
   });
 });
@@ -11,29 +11,58 @@ describe('Get Name', function() {
 
 var stringArr = ['This', 'is', 'to', 'test'];
 
-describe('Get Letters', function() {
-  it('should return total number of letters', function() {
+describe('Get Letters', function () {
+  it('should return total number of letters', function () {
     expect(code.getTotalLetters(stringArr)).toEqual(12);
   });
 });
 
-describe('Create Object', function() {
-  it('should return a new object with a key of the first argument and the value of the second argument', function() {
+describe('Create Object', function () {
+  it('should return a new object with a key of the first argument and the value of the second argument', function () {
     // code.createObject('city', 'Denver');
     expect(code.createObject('city', 'Denver')).toEqual({city: 'Denver'});
   });
 });
 
 var letterArray = ['a', 'b', 'c', 'd', 'e'];
-describe('Negative Index', function() {
-  it('should return the value from the array at the given negative index', function() {
+describe('Negative Index', function () {
+  it('should return the value from the array at the given negative index', function () {
     expect(code.getNegativeIndex(letterArray, -2)).toEqual('d');
   });
 });
 
 var str = 'This is a string';
-describe('Remove Character', function() {
-  it('should remove characters passed as the second argument', function() {
-    expect(code.removeCharacter(str)).toEqual();
+describe('Remove Character', function () {
+  it('should remove characters passed as the second argument', function () {
+    expect(code.removeCharacter(str, 'i')).toEqual('Ths s a strng');
+  });
+  it('should not alter original string', function () {
+    code.removeCharacter(str);
+    expect(str).toEqual('This is a string');
+  });
+});
+
+var newObj = {john: 10, jerry: 11, jenny: 12};
+describe('Output Object', function () {
+  it('should output key and value pairs', function () {
+    expect(code.outputObject(newObj)).toEqual('john is 10, jerry is 11, jenny is 12');
+  });
+});
+
+var testString = "I am the best string!";
+describe('Get Vowels', function () {
+  it('should return an array of all the vowels in a string with no duplicates', function () {
+    expect(code.getVowels(testString)).toEqual(['a', 'e', 'i']);
+  });
+});
+
+var pairedArr = ['a', 'a', 'l', 'l', 'o', 'o'];
+var notPairedArr = ['a', 't', 't', 'r', 'u'];
+describe('Capture Twins', function () {
+  it('should return true if every adjacent pair in array is the same', function () {
+    expect(code.captureTwins(pairedArr)).toEqual(true);
+  });
+  it('should return false if each adjacent pair is not the same', function () {
+    expect(code.captureTwins(notPairedArr)).toEqual(false);
   });
 });
